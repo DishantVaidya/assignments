fae2fbca30fa1dea1108c23478ee9dc57db3a974
Dishant Vaidya <dishantvaidya893@gmail.com>
2025-06-24 01:01:07 -0700

    Update exercise_1.py

--- lecture_1/exercise_1.py
+++ lecture_1/exercise_1.py
@@ -1,24 +1,31 @@
-# This program returns number of prime numbers up to a given number by user input..
-
+#program to print the number of prime numbers given by the user
 try:
-    n = int(input("Enter how many prime numbers you want: "))
-    if n <= 0:
-        print("Enter a positive number please.")
-    else:
-        primes = []
-        num = 2
+    n = int(input("How many prime numbers do you want:"))     #user input for the number of prime numbers to print
 
-        while len(primes) < n:
-            # Assume num is prime
-            prime = True
-            for i in range(2, num):
+    if n <= 0:                                                #checks if the number is less than or equal to 0
+        print("Please enter a number greater than 0.")
+    else:                                                     #continue the loop if the number is greater than 0
+        count = 0                                             
+        num = 2          
+
+        print("Prime numbers are:")                           #print the prime numbers
+       
+        while count < n:                                      # Loop to find and print the first n prime numbers
+
+            
+            prime = True                                      #assume the number is prime initially
+            for i in range(2, num):                           # Loop to check if num is prime
                 if num % i == 0:
                     prime = False
-                    break
-            if prime:
-                primes.append(num)
-            num += 1
 
-        print("Primes:", primes)
-except:
-    print("Please enter a valid integer.")
+            if prime:                                         # If num is prime, it will be printed
+                print(num)
+                count += 1
+            num += 1  
+
+        print("Total prime numbers printed:", count)          #count of prime numbers printed
+
+except ValueError:                                            #exception handling for invalid input
+    print("Invalid input! Please enter a whole number like 5 or 10.")
+
+#end of the program
