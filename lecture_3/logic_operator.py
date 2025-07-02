245ce4376a7ef28424b605dde935c2f58de26976
Dishant Vaidya <dishantvaidya893@gmail.com>
2025-07-02 16:06:39 +0530

    Create logic_operator.py

---
+++ lecture_3/logic_operator.py
@@ -0,0 +1,17 @@
+# Logic Operators Example AND
+a = 11
+b = 22
+c = 30
+
+if (a > b) and (a > c):
+    print("a is the largest")
+
+elif (b > a) and (b > c):
+    print("b is the largest")
+
+else:
+    print("c is the largest")
+
+# Logic Operators Example &
+
+print("A & B :", a & b)
